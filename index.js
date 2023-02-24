ae5193d712cc95d3fdc48af8439021981a3ac59a
Zach Soomro <zachsoomro@Zachs-MacBook-Air.local>
2023-02-23 22:01:01 -0600

    firebase

--- index.js
+++ index.js
@@ -16,3 +16,4 @@ function initMap() {
   
   
   window.initMap = initMap;
+  
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { intializeApp } from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+
+const firebase= intializeApp({
+
+});
