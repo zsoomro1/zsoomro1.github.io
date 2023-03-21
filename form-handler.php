5b6c402a44f8caecd079a2bec63072caafad75b4
Zach Soomro <zachsoomro@Zachs-MacBook-Air.local>
2023-03-21 11:03:11 -0500

    new

--- form-handler.php
+++ form-handler.php
@@ -1,17 +1,18 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$email_from = 'propertymanager@eaglewoodapartments.com';
-$email_subject = 'New Form Submission';
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "Subject: $subject.\n".
-                "User Message: $message.\n";
-$to = 'zachsoomro@gmail.com';
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
-header("Location: contact.html");   
+   $name = $_POST['name'];
+   $visitor_email = $_POST['email'];
+   $subject = $_POST['subject'];
+   $message = $_POST['message'];
+   $email_from = 'propertymanager@greenapartmentswi.com';
+   $email_subject = 'New Form Submission';
+   $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                    "User Message: $message.\n";
+    $to = 'zachsoomro@gmail.com';
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: thanks.html");
 ?>
+
